094906104c00db221c6d97da30e57cc2df4d0034
iamkroot <kroot.patel@gmail.com>
2022-12-09 22:36:49 +0530

    Add Trakt Scrobbler

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,3 +1,9 @@
-const projectsData = []
+const projectsData = [
+  {
+    title: 'Trakt Scrobbler',
+    description: `Python application that automatically recognizes the currently playing media on your PC and syncs that information with Trakt.tv servers.`,
+    href: 'https://github.com/iamkroot/trakt-scrobbler',
+  },
+]
 
 export default projectsData
