a1310a8ef4e01d7f925310b4ff02f74e4925b8c7
iamkroot <kroot.patel@gmail.com>
2023-03-18 17:04:57 +0530

    Add "Post to HackerNews" to PostLayout

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -9,10 +9,11 @@ import Comments from '@/components/comments'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
 
 const editUrl = (fileName) => `${siteMetadata.siteRepo}/blob/master/data/blog/${fileName}`
-const discussUrl = (slug) =>
-  `https://mobile.twitter.com/search?q=${encodeURIComponent(
-    `${siteMetadata.siteUrl}/blog/${slug}`
-  )}`
+const pageUrl = (slug) => encodeURIComponent(`${siteMetadata.siteUrl}/blog/${slug}`)
+
+const twitterUrl = (slug) => `https://mobile.twitter.com/search?q=${pageUrl(slug)}`
+const hnUrl = (slug, title) =>
+  `https://news.ycombinator.com/submitlink?u=${pageUrl(slug)}&t=${encodeURIComponent(title)}`
 
 const postDateTemplate = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
 
@@ -68,17 +69,6 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                       <dl className="whitespace-nowrap text-sm font-medium leading-5">
                         <dt className="sr-only">Name</dt>
                         <dd className="text-gray-900 dark:text-gray-100">{author.name}</dd>
-                        <dt className="sr-only">Twitter</dt>
-                        <dd>
-                          {author.twitter && (
-                            <Link
-                              href={author.twitter}
-                              className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                            >
-                              {author.twitter.replace('https://twitter.com/', '@')}
-                            </Link>
-                          )}
-                        </dd>
                       </dl>
                     </li>
                   ))}
@@ -88,8 +78,12 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
               <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
-                <Link href={discussUrl(slug)} rel="nofollow">
+                <Link href={twitterUrl(slug)} rel="nofollow">
                   {'Discuss on Twitter'}
+                </Link>
+                {` • `}
+                <Link href={hnUrl(slug, title)} rel="nofollow">
+                  {'Post to HackerNews'}
                 </Link>
                 {` • `}
                 <Link href={editUrl(fileName)}>{'View on GitHub'}</Link>
