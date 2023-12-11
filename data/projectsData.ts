374c08d9196148ffbb7d876fe7d2f9171afe86a0
iamkroot <kroot.patel@gmail.com>
2023-12-11 02:04:16 -0600

    Add type to projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,4 +1,11 @@
-const projectsData = [
+interface Projects {
+  title: string,
+  description: string,
+  href?: string,
+  imgSrc?: string,
+}
+
+const projectsData: Projects[] = [
   {
     title: 'Trakt Scrobbler',
     description: `Python application that automatically recognizes the currently playing media on your PC and syncs that information with Trakt.tv servers.`,
