53e542fc608275517f3f9b91f9b0b346f03098f9
iamkroot <kroot.patel@gmail.com>
2023-12-18 00:19:53 -0500

    Image: add size manually if static file

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,6 +1,20 @@
 import NextImage, { ImageProps } from 'next/image'
+import { imageSize } from 'image-size'
 
 // const Image = ({ ...rest }) => <img {...rest} />
-const Image = ({ src, ...rest }: ImageProps) => <NextImage src={src} {...rest} />
+const Image = ({ src, ...rest }: ImageProps) => {
+  let width: number | undefined = undefined,
+    height: number | undefined = undefined
+  if (
+    process.env.NODE_ENV === 'development' &&
+    !('width' in rest) &&
+    src.toString().startsWith('/static')
+  ) {
+    const size = imageSize('public' + src.toString())
+    height = size.height
+    width = size.width
+  }
+  return <NextImage src={src} width={width} height={height} {...rest} />
+}
 
 export default Image
